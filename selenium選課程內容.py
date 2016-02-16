956fd8098c2b0f72c03e48d9998c85d09867bc5d
nps798 <nps798@gmail.com>
2016-02-16 18:53:14 +0800

    160216 1851 選課程內容
    Selenium 一定要開啟 webdriver 才能讀取文字
    因此放棄採用selenium
    改用BeautifulSoup

--- selenium選課程內容.py
+++ selenium選課程內容.py
@@ -1,5 +1,3 @@
-from selenium import webdriver
-
-driver.get("http://140.116.165.74/qry/qry001.php?dept_no=")
-element = driver.find_element_by_xpath("/html/body/table/tbody/tr[1]")
-print(element.text)
+import requests
+from bs4 import BeautifulSoup
+import re
